e07eb840d11d22a0eaea4994795284f587eef63a
BORIS\Boris <b.zivkovic97@yahoo.com>
2022-07-16 17:17:17 +0200

    implemented stripe payment, Stripe_secret_key, and React_app_stripe_publishable, deploying the app

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,7 +1,8 @@
 import {
   BaseButton,
   GoogleSignInButton,
-  InvertedButton
+  InvertedButton,
+  ButtonSpinner
 } from './button.styles';
 
 export const BUTTON_TYPE_CLASSES = {
@@ -18,10 +19,12 @@ const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) => (
   }[buttonType]
 )
 
-const Button = ({ children, buttonType, ...otherProps }) => {
+const Button = ({ children, buttonType, isLoading, ...otherProps }) => {
   const CustomButton = getButton(buttonType);
   return (
-    <CustomButton {...otherProps} > {children} </CustomButton>
+    <CustomButton disabled={isLoading} {...otherProps} >
+      {isLoading ? <ButtonSpinner/> : children}
+    </CustomButton>
   );
 };
 
